3a5fef86502c561126a90ab9df3b132091ed915c
Alexandr Machilov <machilov00@mail.ru>
2022-03-12 16:24:16 +0300

    fix: app

--- app.js
+++ app.js
@@ -2,26 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { PORT = 3000 } = process.env;
 const app = express();
-//логгер
+
 app.use((req, res, next) => {
   console.log(req.method, req.path);
   next();
 });
 
-//разобраться что это делает
 app.use(express.json());
 
-//front
-app.get("/", (req, res) => {
-  res.send("Hello World!A I know you miss me");
-});
-
-//подключение к базе данных
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/mestodb", {
     useNewUrlParser: true,
-    /* useCreateIndex: true,
-    useFindAndModify: false, */
   });
 
   app.listen(PORT, () => {
